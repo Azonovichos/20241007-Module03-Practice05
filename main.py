401887e92510f359638a5ff3ba99fe8cc60baa81
Marsel <kms-official@yandex.ru>
2024-10-09 13:35:16 +0300

    Patch_1.1
    Скорректировано для условия на ввод числа, оканчивающегося на "0"

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ def get_multiplied_digits (number):
     first = int(str_number[0])
 
     if len(str_number) > 1:
+
         return first * get_multiplied_digits(int(str_number[1:]))
+
+    elif first == 0:
+        return first + 1
+
     else:
         return first
 
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(402030)
 print(result)
+
+'''number = 40203
+str_ = str(number)
+print(str(str_[-1]))'''
